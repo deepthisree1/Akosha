e2fb7b474e4c58645eee5e7c8d60f58528e726fa
Deepthi Sree Vamaraju <deepthi.vamaraju@gmail.com>
2015-06-14 22:38:57 +0800

    Update Main_Model.py

--- Model/Main_Model.py
+++ Model/Main_Model.py
@@ -8,6 +8,8 @@ import nltk
 from nltk.tokenize import word_tokenize 
 from sklearn.externals import joblib
 
+#reading the data and cleaning
+
 def read_data():
     with open("/Users/deepthi/Downloads/as.txt", 'r') as fileinput:
         lines= [filter(lambda x: x in string.printable, (re.sub(r"(?:\@|https?\://)\S+", "", line)).lower().translate(None, string.punctuation)) for line in fileinput]
@@ -15,12 +17,13 @@ def read_data():
     tokenize = [word_tokenize(text)  for text in lines]
     return lines,tokenize
     
-
+# finding the similar data
 def similar(data):
     for tokens in data:
         verb = [toks[0] for toks in nltk.pos_tag(tokens) if toks[1] in ['VB','JJ']]
     print verb
-    
+
+# bigrams and their conditional probabilities
 def create_model(data):
     unigrams=[]
     for words in data:
@@ -28,7 +31,8 @@ def create_model(data):
             unigrams.append(tokens)
     cfreq_data_bigram = nltk.ConditionalFreqDist(nltk.bigrams((unigrams)))
     return cfreq_data_bigram
-
+    
+# traing the model with bigrams and their conditional probabilities
 def main_code():
     data,tokenized_data = read_data()
     joblib.dump(data,"Data_Model.pkl")
